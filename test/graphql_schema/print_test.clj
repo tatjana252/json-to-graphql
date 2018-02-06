52b682f3488ad1b0f985ce778f381c6e52d71d7d
tatjana252 <tanja.s@hotmail.com>
2018-02-06 18:42:37 +0100

    Initial commit

--- test/graphql_schema/print_test.clj
+++
@@ -1,6 +0,0 @@
-(ns graphql-schema.print-test
-  (:require [clojure.test :refer :all])
-  (:require [graphql-schema.print :refer [object-type]]))
-
-(deftest object-type-test
-  )
--- test/graphql_schema/value_test.clj
+++
@@ -1,3 +0,0 @@
-(ns graphql-schema.value-test
-  (:require [clojure.test :refer :all])
-  (:require [graphql-schema.value :refer [scalar-type]]))
